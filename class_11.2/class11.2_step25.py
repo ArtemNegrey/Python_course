64fc4bd17aa52584777b24e9364437d8ab1fbb0c
Artem <aartnegrey@gmail.com>
2026-04-03 13:28:29 +0300

    add new class 11.2

---
+++ class_11.2/class11.2_step25.py
@@ -0,0 +1,5 @@
+# Дополните приведённый ниже код так, чтобы он вывел "перевёрнутый" список languages (то есть элементы будут идти в обратном порядке).
+
+languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'Lahnda']
+
+print(languages[::-1])
