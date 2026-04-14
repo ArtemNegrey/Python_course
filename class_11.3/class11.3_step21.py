b43b58be6b191450fdb0064ecad78a53d7508e71
Artem <aartnegrey@gmail.com>
2026-04-14 12:25:04 +0300

    add class 11.3 step 21

--- class_11.3/class11.3_step21.py
+++ class_11.3/class11.3_step21.py
@@ -1,2 +1,18 @@
 # На вход программе подаются натуральное число n и n строк, а затем число k. 
 # Напишите программу, которая выводит k-ую букву из введённых строк на одной строке без пробелов.
+
+n = int(input())
+cloud = []
+
+for i in range(n):
+    cloud.append(input())
+
+k = int(input())
+
+result = ''
+
+for s in cloud:
+    if len(s) >= k:
+        result += s[k - 1]
+
+print(result)
